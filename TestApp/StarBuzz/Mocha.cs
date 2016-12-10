eea6c280d0fe4c8cb46ba982227fd1eb42d877ee
rbell <rbell@PMI0565>
2016-12-10 13:54:01 -0500

    Starbuzz

---
+++ TestApp/StarBuzz/Mocha.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StarBuzz
+{
+    public class Mocha : CondimentDecorator
+    {
+        public Beverage beverage;
+
+        public Mocha(Beverage beverage)
+        {
+            this.beverage = beverage;
+        }
+
+        public override string getDescription()
+        {
+            return beverage.getDescription() + ", Mocha";
+        }
+
+        public override double cost()
+        {
+            return beverage.cost() + .20;
+        }
+    }
+}
---
+++ TestApp/StarBuzz/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StarBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Beverage bev = new Espresso();
+            Console.WriteLine(bev.getDescription() + " $" + bev.cost());
+
+            Beverage bev2 = new DarkRoast();
+            bev2 = new Mocha(bev2);
+            bev2 = new Mocha(bev2);
+            bev2 = new Whip(bev2);
+            Console.WriteLine(bev2.getDescription() + " $" + bev2.cost());
+
+            Beverage bev3 = new HouseBlend();
+            bev3 = new Soy(bev3);
+            bev3 = new Mocha(bev3);
+            bev3 = new Whip(bev3);
+            Console.WriteLine(bev3.getDescription() + " $" + bev3.cost());
+        }
+    }
+}
